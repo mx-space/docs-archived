51df9443e8961bfdd7fe3336ff701836f5a544b8
wibus-wee <1596355173@qq.com>
2022-04-03 07:53:00 +0800

    feat: home component

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,18 +1,47 @@
 import { defineConfigWithTheme } from "vitepress";
 import type { Config as ThemeConfig } from '@vue/theme'
 // @ts-ignore
-import baseConfig from '@vue/theme/config'
+import base from '@vue/theme/config'
+
+const Sidebar = [
+  { text: '介绍', link: '/introduce/' },
+  { text: '部署', link: '/deploy/' },
+
+  // 另外一种写法，不过推荐将items内的东西另外定义一个常量，利于修改
+  // { text: '介绍', items: [
+  //   { text: '构建', link: '/introduce/build/' },
+  // ]
+  // },
+]
+
 
 export default defineConfigWithTheme<ThemeConfig>({
-  extends: baseConfig,
+  extends: base,
   
-  lang: 'zh-CN',
   title: 'Mix Space',
+  description: 'An alternative personal space.',
+  lang: 'zh-CN',
   srcDir: 'src',
   scrollOffset: 'header',
 
   themeConfig: {
 
+    editLink: {
+      repo: 'mx-space/kami',
+      text: 'Edit this page'
+    },
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/mx-space' },
+    ],
+
+    nav: [
+      { text: '介绍', link: '/introduce/'},
+      { text: '部署', link: '/deploy/' },
+      { text: '设置', link: '/setting/' },
+      { text: '帮助', link: '/help/' },
+      { text: '开发', link: '/dev/' },
+    ],
 
     footer: {
       license: {
