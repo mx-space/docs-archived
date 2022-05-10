d8028592ee199733d4fa1e84065b3d6138bf1df8
Timochan <teemo@timochan.cn>
2022-05-10 21:37:34 +0800

    chore : update for doc edit

--- src/.vitepress/config.ts
+++ src/.vitepress/config.ts
@@ -78,11 +78,11 @@ const config: UserConfig = {
   scrollOffset: 'header',
 
   themeConfig: {
-    // editLink: {
-    //   repo: 'mx-space/docs',
-    //   text: '编辑此页',
-    // },
-    // editLinks: false,
+    editLink: {
+      repo: 'mx-space/docs',
+      text: '编辑此页',
+    },
+    editLinks: true,
 
     algolia: {
       apiKey: '17c9be610f7f8fac9911a340c35ef34f',
