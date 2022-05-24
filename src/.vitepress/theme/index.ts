14a3f1cad57398c7a0e1edec0997e8296ce5a884
Timochan <teemo@timochan.cn>
2022-05-24 18:19:29 +0800

    Merge branch 'main' of https://github.com/mx-space/docs

--- src/.vitepress/theme/index.ts
+++ src/.vitepress/theme/index.ts
@@ -1,12 +1,23 @@
 import { VPTheme } from '@vue/theme'
 import { createHead } from '@vueuse/head'
-import { App, h } from 'vue'
+import { useRouter } from 'vitepress'
+import { App, h, watch } from 'vue'
 // @ts-ignore
 import Banner from './components/Banner.vue'
 import './styles/code.css'
 
 export default Object.assign({}, VPTheme, {
   Layout: () => {
+    const router = useRouter()
+
+    watch(
+      () => router.route.data.relativePath,
+      (path) => {
+        ;(window as any).umami.trackView(path)
+        ;(window as any).umami.trackEvent(router.route.data.title, 'view')
+      },
+      { immediate: true }
+    )
     // @ts-ignore
     return h(VPTheme.Layout, null, {
       banner: () => h(Banner),
